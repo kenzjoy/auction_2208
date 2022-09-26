49d69f8f72037b79a412d488fa560b75be03944d
Mackenzie Leng <kenz.joy@outlook.com>
2022-09-26 10:09:03 -0600

    feat: add potential_revenue test before creating method

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -79,5 +79,21 @@ RSpec.describe Auction do
     end
   end
 
+  describe '#potential_revenue' do
+    it 'returns the potential revenue from an auction' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      @item4.add_bid(@attendee3, 50)
+      @item3.add_bid(@attendee2, 15)
+
+      expect(@auction.potential_revenue).to eq(87)
+    end
+  end
+
   
 end
