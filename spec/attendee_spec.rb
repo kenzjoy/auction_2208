66e48c9a3a0f210b37318d4bb4fe05cf7398335c
Mackenzie Leng <kenz.joy@outlook.com>
2022-09-26 09:14:32 -0600

    feat: initialize attendee class

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -1,3 +1,16 @@
 require './lib/item'
 require './lib/auction'
 require './lib/attendee'
+
+RSpec.describe Attendee do
+  before(:each) do
+    @attendee = Attendee.new( {
+                    name: 'Megan', 
+                    budget: '$50'
+                } )
+  end
+
+    it 'exists' do
+      expect(@attendee).to be_an(Attendee)
+    end
+end
