8e85ab3248961c1862792de4401fa436419ced24
Mackenzie Leng <kenz.joy@outlook.com>
2022-09-26 11:11:10 -0600

    feat: set up expected hash for bidder_info test

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -134,4 +134,39 @@ RSpec.describe Auction do
       expect(@auction.bidders).to eq(['Megan', 'Bob', 'Mike'])
     end
   end
+
+  describe '#bidder_info' do 
+    it 'returns a hash with keys that are attendees, and values that
+    are a hash with that attendees budgets and an array of items that
+    attendee has bid on' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+      @item1.add_bid(@attendee1, 22)
+      @item1.add_bid(@attendee2, 20)
+      @item4.add_bid(@attendee3, 50)
+      @item3.add_bid(@attendee2, 15)
+      @item1.close_bidding
+      @item1.add_bid(@attendee3, 70)
+
+      expected = {
+        @attendee1 => {
+                       :budget => 50,
+                       :items => [@item1]
+                      },
+        @attendee2 => {
+                       :budget => 75,
+                       :items => [@item1, @item3]
+                      },
+        @attendee3 => {
+                       :budget => 100,
+                       :items => [@item4]
+                      }
+      }
+
+      expect(@auction.bidder_info).to eq(expected)
+    end
+  end
 end
