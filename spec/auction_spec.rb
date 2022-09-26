85c9a0581420aa5855326bfdd58049469017bbec
Mackenzie Leng <kenz.joy@outlook.com>
2022-09-26 09:21:08 -0600

    feat: initalize auction class

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -1,3 +1,13 @@
 require './lib/item'
 require './lib/auction'
 require './lib/attendee'
+
+RSpec.describe Auction do
+  before(:each) do
+    @auction = Auction.new
+  end
+
+    it 'exists' do 
+      expect(@auction).to be_an(Auction)
+    end
+end
