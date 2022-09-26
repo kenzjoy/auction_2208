bb767d7612fbb841278c9003c3f75a621faa147b
Mackenzie Leng <kenz.joy@outlook.com>
2022-09-26 09:11:16 -0600

    feat: complete item class setup

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -5,9 +5,14 @@ require './lib/attendee'
 RSpec.describe Item do
   before(:each) do
     @item1 = Item.new('Chalkware Piggy Bank')
+    @item2 = Item.new('Bamboo Picture Frame')
   end
 
     it 'exists' do
       expect(@item1).to be_an(Item)
+    end
+
+    it 'has attributes' do 
+      expect(@item1.name).to eq('Chalkware Piggy Bank')
     end
 end
